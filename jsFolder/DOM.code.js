1d8db0f8aeb7fc929dc6767e9474f68bbf0d9a35
Viacheslav Surovets <1844zaratustra@gmail.com>
2018-05-27 23:40:57 +0300

    indexPage

--- jsFolder/DOM.code.js
+++ jsFolder/DOM.code.js
@@ -2,4 +2,14 @@
  * Created by Слава on 25.05.18.
  */
 'use strict';
+//window.history.pushState("object or string", "Title", "Thank U for watching");
+function changeHash(id) {
+        try {
+            history.replaceState(null, null, '?id=' + id);
+        }
+        catch (e) {
+            location.hash = '#id_' + id;
+        }
 
+    };
+changeHash("Thank U for watching!");
