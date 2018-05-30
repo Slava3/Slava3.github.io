3d5d0fa06969eb1c0c48a855fff92546a2a0bb1c
Viacheslav Surovets <1844zaratustra@gmail.com>
2018-05-31 00:17:16 +0300

    Boat done!

--- jsFolder/logic.js
+++ jsFolder/logic.js
@@ -2,3 +2,14 @@
  * Created by Слава on 25.05.18.
  */
 'use strict';
+
+
+$(function  () {
+   $('<p>').appendTo('picture').addClass('text_for_boat').text('Hello World Hello World Hello World Hello World')
+});
+
+
+
+
+
+
